f335a8e76516c7d87b33775bcbb104be52a77b73
Yash Menaria <107399779+Yashmenaria1@users.noreply.github.com>
2023-06-11 10:36:26 +0530

    Update 1146. Snapshot Array.cpp

--- 1146. Snapshot Array.cpp
+++ 1146. Snapshot Array.cpp
@@ -1,29 +1,24 @@
 class SnapshotArray {
- public:
-  SnapshotArray(int length) : snaps(length) {
-    for (auto& snap : snaps)
-      snap.emplace_back(0, 0);
-  }
-
-  void set(int index, int val) {
-    auto& [lastSnapId, lastVal] = snaps[index].back();
-    if (lastSnapId == snap_id)
-      lastVal = val;
-    else
-      snaps[index].emplace_back(snap_id, val);
-  }
-
-  int snap() {
-    return snap_id++;
-  }
-
-  int get(int index, int snap_id) {
-    const vector<pair<int, int>>& snap = snaps[index];
-    auto it = lower_bound(begin(snap), end(snap), make_pair(snap_id + 1, 0));
-    return prev(it)->second;
-  }
-
- private:
-  vector<vector<pair<int, int>>> snaps;
-  int snap_id = 0;
+    vector<vector<pair<int, int>>> snaps;
+    int curSnap;
+public:
+    SnapshotArray(const int& length) {
+        snaps.resize(length, {{-1, 0}});
+        curSnap = 0;
+    }
+    
+    void set(const int& index, const int& val) { 
+        if (snaps[index].back().first < curSnap)
+            snaps[index].push_back({curSnap, val});
+        else
+            snaps[index].back().second = val;
+    }
+    
+    int snap() {
+        return curSnap++;
+    }
+    
+    int get(const int& index, const int& snap_id) {
+        return (upper_bound(snaps[index].begin(), snaps[index].end(), make_pair(snap_id, INT_MAX)) - 1)->second;
+    }
 };
