f57f4dde23670c26b73bbb90e6d737e4d48958af
Yash Menaria <107399779+Yashmenaria1@users.noreply.github.com>
2023-08-02 10:40:28 +0530

    Create 46. Permutations.cpp

---
+++ 46. Permutations.cpp
@@ -0,0 +1,21 @@
+class Solution {   
+private :
+    void permute(vector<int> &nums, vector<vector<int>> &ans, vector<int> &temp, int index){
+        if(index == nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=index; i<temp.size(); i++){
+            swap(temp[index] , temp[i]);
+            permute(nums, ans, temp, index+1);
+            swap(temp[index] , temp[i]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp = nums;
+        permute(nums, ans, temp, 0);
+        return ans;
+    }
+};
