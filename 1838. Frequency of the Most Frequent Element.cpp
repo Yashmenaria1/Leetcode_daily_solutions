886c34ec42db6dd444424daf0022b5c09bd2f142
Yash Menaria <107399779+Yashmenaria1@users.noreply.github.com>
2023-11-18 09:39:31 +0530

    Create 1838. Frequency of the Most Frequent Element.cpp

---
+++ 1838. Frequency of the Most Frequent Element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        long i = 0, ans = 1, sum = 0;
+        for (int j = 0; j < nums.size(); ++j) {
+            sum += nums[j];
+            while ((j - i + 1) * nums[j] - sum > k) sum -= nums[i++];
+            ans = max(ans, j - i + 1);
+        }
+        return ans;
+    }
+};
