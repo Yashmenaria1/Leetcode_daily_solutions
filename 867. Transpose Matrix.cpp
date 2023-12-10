999a71bed59f38f056b943c38d62ddffc8458a67
Yash Menaria <107399779+Yashmenaria1@users.noreply.github.com>
2023-12-10 08:14:45 +0530

    Update 867. Transpose Matrix.cpp

--- 867. Transpose Matrix.cpp
+++ 867. Transpose Matrix.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>> matrix) {
+        if (matrix.empty() || matrix[0].empty()) {
+            return {};
+        }
+
+        int numRows = matrix.size(), numCols = matrix[0].size();
+        vector<vector<int>> res(numCols, vector<int>(numRows, 0));
+
+        for (int j = 0; j < numCols; j++) {
+            for (int i = 0; i < numRows; i++) {
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>> matrix) {
         int A = matrix.size(), B = matrix[0].size();
         vector<vector<int>> res(B, vector<int>(A, 0));
         for (int j = 0; j < B; j++)
