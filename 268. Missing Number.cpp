fb712eeceb2ce57e6a6fd48138570c6a10b280d6
Yash Menaria <107399779+Yashmenaria1@users.noreply.github.com>
2024-02-20 08:52:22 +0530

    Create 268. Missing Number.cpp

---
+++ 268. Missing Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int expectedSum = (n *(n + 1)) / 2;
+        int actualSum = 0;
+        for (int i = 0; i < n; i++){
+            actualSum += nums[i];
+        }
+        return expectedSum - actualSum;
+    }
+};
