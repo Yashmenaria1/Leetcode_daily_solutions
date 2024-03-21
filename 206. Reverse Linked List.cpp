80072bcf86965ab03637cbc15b93ec872ecf45f9
Yash Menaria <107399779+Yashmenaria1@users.noreply.github.com>
2024-03-21 08:27:44 +0530

    Create 206. Reverse Linked List.cpp

---
+++ 206. Reverse Linked List.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* &head) {
+        if (head == NULL||head->next==NULL) return head;
+
+        ListNode* node = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return node;
+    }
+};
