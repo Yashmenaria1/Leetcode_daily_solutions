cea6c4a8b0b08fca6b69bd5ecd00ca7ae23afa82
Yash Menaria <107399779+Yashmenaria1@users.noreply.github.com>
2024-06-15 09:14:19 +0530

    Create 502. IPO.cpp

---
+++ 502. IPO.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size();
+        vector<pair<int, int>> projects(n);
+        for (int i = 0; i < n; i++) {
+            projects[i] = {capital[i], profits[i]};
+        }
+        sort(projects.begin(), projects.end());
+        int i = 0;
+        priority_queue<int> maxCapital;
+        while (k--) {
+            while (i < n && projects[i].first <= w) {
+                maxCapital.push(projects[i].second);
+                i++;
+            }
+            if (maxCapital.empty())
+                break;
+            w += maxCapital.top();
+            maxCapital.pop();
+        }
+        return w;
+    }
+};
