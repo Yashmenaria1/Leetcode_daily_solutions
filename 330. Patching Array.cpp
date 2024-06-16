4e7ea79b21ffdc5997ceac54c994e9b955924181
Yash Menaria <107399779+Yashmenaria1@users.noreply.github.com>
2024-06-16 09:36:41 +0530

    Create 330. Patching Array.cpp

---
+++ 330. Patching Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long miss = 1;
+        int res = 0;
+        size_t i = 0;
+
+        while (miss <= n) {
+            if (i < nums.size() && nums[i] <= miss) {
+                miss += nums[i];
+                i++;
+            } else {
+                miss += miss;
+                res++;
+            }
+        }
+        return res;
+    }
+};
