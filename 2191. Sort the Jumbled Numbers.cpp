f9963af65ff7b45e370394765f552a2abb5f1893
Yash Menaria <107399779+Yashmenaria1@users.noreply.github.com>
2024-07-24 15:01:32 +0530

    Update 2191. Sort the Jumbled Numbers.cpp

--- 2191. Sort the Jumbled Numbers.cpp
+++ 2191. Sort the Jumbled Numbers.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        int na = nums.size();
+        vector<pair<int, int>> mpp(na);
+        vector<int> ans;
+        for(int i = 0; i<nums.size(); i++){
+            int n = nums[i];
+            int num = n==0?mapping[0]:0;
+            int place = 1;
+            while(n){
+                int digit = n%10;
+                num += mapping[digit]*place;
+                n/=10;
+                place *= 10;
+            }
+            mpp[i] = {num, i};
+        }
+        sort(mpp.begin(), mpp.end());
+        for(auto m:mpp){
+            ans.push_back(nums[m.second]);
+        }
+        return ans;
+    }
+};
