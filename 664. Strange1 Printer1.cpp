dafee092b0f60d934dc87463f65efdec89d6bc1f
Yash Menaria <107399779+Yashmenaria1@users.noreply.github.com>
2024-08-21 18:53:10 +0530

    Create 664. Strange1 Printer1.cpp

---
+++ 664. Strange1 Printer1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int strangePrinter(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+
+        for (int l = 1; l <= n; ++l) {
+            for (int i = 0; i <= n - l; ++i) {
+                int j = i + l - 1;
+                if (i == j) {
+                    dp[i][j] = 1;
+                } 
+                else {
+                    if (s[i] == s[j]) {
+                        dp[i][j] = dp[i][j - 1];
+                    } 
+                    else {
+                        dp[i][j] = INT_MAX;
+                        for (int k = i; k < j; ++k) {
+                            dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j]);
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+};
